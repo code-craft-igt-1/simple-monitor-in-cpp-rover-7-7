bc3a1710752d142f6303ea901773d7ef3d2558c4
Prince Kumar <Prince.Kumar@Philips.com>
2024-09-01 16:45:15 +0530

    Code Complexity Reduction

--- monitor.cpp
+++ monitor.cpp
@@ -1,38 +1,52 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+
+
+using std::cout;
+using std::flush;
+using std::this_thread::sleep_for;
+using std::chrono::seconds;
+
+void displayAlert(const std::string& message) {
+    cout << message << "\n";
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+}
+
+bool checkTemperature(float temperature) {
+    if (temperature > 102 || temperature < 95) {
+        displayAlert("Temperature is critical!");
+        return false;
+    }
+    return true;
+}
+
+bool checkPulseRate(float pulseRate) {
+    if (pulseRate < 60 || pulseRate > 100) {
+        displayAlert("Pulse Rate is out of range!");
+        return false;
+    }
+    return true;
+}
+
+bool checkSpo2(float spo2) {
+    if (spo2 < 90) {
+        displayAlert("Oxygen Saturation out of range!");
+        return false;
+    }
+    return true;
+}
+
+bool areVitalsOk(float temperature, float pulseRate, float spo2) {
+    return checkTemperature(temperature) && checkPulseRate(pulseRate) && checkSpo2(spo2);
+}
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
+    return areVitalsOk(temperature, pulseRate, spo2) ? 1 : 0;
 }
