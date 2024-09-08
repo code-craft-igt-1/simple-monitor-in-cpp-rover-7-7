d755a9b5b71e23eac82420dd34d070a1bd53564c
Prince Kumar <Prince.Kumar@Philips.com>
2024-09-08 09:02:26 +0530

    Added Health Monitor Extensions

--- monitor.h
+++ monitor.h
@@ -1,3 +1,43 @@
-#pragma once
+#ifndef MONITOR_H_
+#define MONITOR_H_
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+#include <unordered_map>
+#include <string>
+
+using Messages = std::unordered_map<std::string, std::unordered_map<std::string, std::string>>;
+
+extern Messages messages;
+
+void displayAlert(const std::string& messageKey, const std::string&
+language, const Messages& messages);
+bool isUpperWarning(float temperature, float upperWarningLimit, float TEMP_UPPER_LIMIT);
+bool isLowerWarning(float temperature, float lowerWarningLimit, float TEMP_LOWER_LIMIT);
+bool isTemperatureunder(float temperature, const std::string& language,
+const Messages& messages);
+bool isTemperatureover(float temperature, const std::string& language,
+const Messages& messages);
+bool isTemperatureCritical(float temperature);
+bool isTemperatureWarning(float temperature);
+bool checkTemperature(float temperature, const std::string& language,
+const Messages& messages);
+void displayTemperatureWarning(float temperature, const std::string& language,
+const Messages& messages);
+bool isPulseRateHighWarning(float pulseRate);
+bool isPulseRateLowWarning(float pulseRate);
+bool isPulseRateCritical(float pulseRate);
+void displayPulseRateWarning(float pulseRate, const std::string& language,
+const Messages& messages);
+void handlePulseRateWarning(float pulseRate, const std::string& language, const Messages& messages);
+bool isPulseRateWarning(float pulseRate);
+bool checkPulseRate(float pulseRate, const std::string& language,
+const Messages& messages);
+bool isSpo2Critical(float spo2);
+bool isSpo2Warning(float spo2);
+bool checkSpo2(float spo2, const std::string& language,
+const Messages& messages);
+int vitalsOk(float temperature, float pulseRate,
+float spo2, const std::string& language, const Messages& messages);
+bool areVitalsOk(float temperature, float pulseRate,
+float spo2, const std::string& language, const Messages& messages);
+
+#endif  // MONITOR_H_
--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,8 +1,60 @@
+#include <unordered_map>
+#include <string>
+#include "gtest/gtest.h"
 #include "./monitor.h"
 
-#include "gtest/gtest.h"
-
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+    ASSERT_FALSE(areVitalsOk(99, 102, 70, "EN", messages));
+    ASSERT_TRUE(areVitalsOk(98.1, 70, 98, "EN", messages));
+}
+
+TEST(Monitor, OkWhenAllVitalsAreInRange) {
+    ASSERT_TRUE(areVitalsOk(100, 99, 91, "EN", messages));
+    ASSERT_TRUE(areVitalsOk(98.6, 90, 95, "EN", messages));
+    ASSERT_TRUE(areVitalsOk(97.5, 80, 100, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenTemperatureIsOutOfRange) {
+    ASSERT_FALSE(areVitalsOk(95, 100, 80, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(105, 100, 80, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenPulseRateIsOutOfRange) {
+    ASSERT_FALSE(areVitalsOk(100, 60, 80, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(100, 120, 80, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenBloodPressureIsOutOfRange) {
+    ASSERT_FALSE(areVitalsOk(100, 100, 60, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(100, 100, 55, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenHeartRateIsBelowRange) {
+    ASSERT_FALSE(areVitalsOk(95, 100, 80, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(97.5, 101, 95, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenHeartRateIsAboveRange) {
+    ASSERT_FALSE(areVitalsOk(105, 100, 80, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(102.5, 90, 95, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenOxygenLevelIsBelowRange) {
+    ASSERT_FALSE(areVitalsOk(100, 95, 80, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(98.6, 85, 89, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenOxygenLevelIsAboveRange) {
+    ASSERT_FALSE(areVitalsOk(100, 105, 80, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(98.6, 100, 69, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenTemperatureIsBelowRange) {
+    ASSERT_FALSE(areVitalsOk(100, 100, 75, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(92.8, 90, 97.5, "EN", messages));
+}
+
+TEST(Monitor, NotOkWhenTemperatureIsAboveRange) {
+    ASSERT_FALSE(areVitalsOk(100, 100, 85, "EN", messages));
+    ASSERT_FALSE(areVitalsOk(103, 90, 100, "EN", messages));
 }
